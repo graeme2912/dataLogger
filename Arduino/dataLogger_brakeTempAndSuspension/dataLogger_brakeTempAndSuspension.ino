1cd6250f628a2cff498b31e8dffe3f5d5f9bf5a5
Graeme <graemedcheyne@hotmail.com>
2019-10-16 11:49:01 +0100

    added combined brake temp and suspension sensor program, untested.

--- Arduino/dataLogger_brakeTempAndSuspension/dataLogger_brakeTempAndSuspension.ino
+++ Arduino/dataLogger_brakeTempAndSuspension/dataLogger_brakeTempAndSuspension.ino
@@ -11,15 +11,29 @@ MLX90614 ------------- Arduino
 #include <SparkFunMLX90614.h> // SparkFunMLX90614 Arduino library
 #include <HCRTC.h> //Include the Hobby Components RTC library
 #include <Adafruit_SD.h> //Include the adafruit modified SD card library, for compatability with the arduino mega
+#include "Adafruit_VL53L0X.h" //time of flight sensor library
+#include <Adafruit_Sensor.h>
 
-#define NUMBER_OF_SENSORS 5 //for some reason the last sensor always fails when logging or reading rtc so we need 5
+#define NUMBER_OF_THERM_SENSORS 5 //for some reason the last sensor always fails when logging or reading rtc so we need 5
+#define NUMBER_OF_SUSP_SENSORS 4
 #define I2CDS1307Add 0x68 //real time clock fixed address
 
-unsigned int LOX_ADDRESSES[] = { 0x5A, 0x5B, 0x5C, 0x5D, 0x5E }; //addresses of each sensor
+unsigned int THERM_ADDRESSES[] = { 0x5A, 0x5B, 0x5C, 0x5D, 0x5E }; //addresses of each sensor
+unsigned int LOX_ADDRESSES[] = { 0x30, 0x31, 0x32, 0x33 }; //suspension sensors addressses
+unsigned int SHT_LOX[] = { 7, 6, 5, 3 }; //shutdown pins
 
-bool verbose = false; //enable extended serial output for debugging
+bool verbose = true; //enable extended serial output for debugging
 
-IRTherm therm[NUMBER_OF_SENSORS - 1]; //Create multiple IRTherm objects to interact with throughout
+IRTherm therm[NUMBER_OF_THERM_SENSORS - 1]; //Create multiple IRTherm objects to interact with throughout
+
+											// objects for the vl53l0x
+Adafruit_VL53L0X lox[NUMBER_OF_SUSP_SENSORS - 1];
+
+//for some reason this has to stay in for it to work, not sure why
+Adafruit_VL53L0X lox1 = Adafruit_VL53L0X();
+
+// this holds the measurement
+VL53L0X_RangingMeasurementData_t measure[NUMBER_OF_SUSP_SENSORS - 1];
 
 HCRTC HCRTC; //real time clock library
 
@@ -29,8 +43,8 @@ void enable_sensors() {
 	float emis = 0.7; //emissivity of material 
 
 	if (verbose) Serial.println("enabling sensors");
-	for (int i = 0; i < NUMBER_OF_SENSORS; i++) {
-		therm[i].begin(LOX_ADDRESSES[i]); //enable sensors
+	for (int i = 0; i < NUMBER_OF_THERM_SENSORS; i++) {
+		therm[i].begin(THERM_ADDRESSES[i]); //enable sensors
 		therm[i].setUnit(TEMP_C); //set unit to degrees celcius
 		//if (!therm[i].setEmissivity(emis)) while(1); //set emissivity co-efficient to emis
 		
@@ -42,7 +56,7 @@ void enable_sensors() {
 			Serial.print("Sensor enabled: ");
 			Serial.print(i);
 			Serial.print("   at address: ");
-			//Serial.print(LOX_ADDRESSES[i]);
+			//Serial.print(THERM_ADDRESSES[i]);
 			Serial.print(therm[i].readAddress()); //will return 0 of the sensor fails to initialize
 			Serial.print(" with emissivity: ");
 			Serial.print(therm[i].readEmissivity()); //will return 0 of the sensor fails to initialize
@@ -50,6 +64,8 @@ void enable_sensors() {
 		}
 	delay(200);
 	}
+
+	setup_suspension_sensors();
 }
 
 void setup_SD_card() {
@@ -70,8 +86,88 @@ void setup_SD_card() {
 	}
 }
 
+void reset_all(bool do_reset) {
+	for (int i = 0; i < NUMBER_OF_SUSP_SENSORS; i++) {
+		if (do_reset) {
+			digitalWrite(SHT_LOX[i], LOW);
+			if (verbose) {
+				Serial.print("Set sensor low: ");
+				Serial.println(i);
+			}
+		}
+		else {
+			digitalWrite(SHT_LOX[i], HIGH);
+			if (verbose) {
+				Serial.print("Set sensor high: ");
+				Serial.println(i);
+			}
+		}
+	}
+	delay(10);
+}
+
+void enable_sensor(int num) {
+	digitalWrite(SHT_LOX[num], HIGH);
+	if (verbose) {
+		Serial.print("Set sensor high: ");
+		Serial.println(num);
+	}
+	delay(10);
+
+	//initing LOX
+	while (!lox[num].begin(LOX_ADDRESSES[num])) {
+		if (verbose) {
+			Serial.print(F("Failed to boot VL53L0X number"));
+			Serial.println(num);
+			delay(100);
+			//while (1);
+		}
+	}
+}
+
+void setID() {
+	reset_all(true);
+	reset_all(false);
+
+	for (int i = 0; i < NUMBER_OF_SUSP_SENSORS; i++) {
+		for (int j = i + 1; j < NUMBER_OF_SUSP_SENSORS; j++) {
+			digitalWrite(SHT_LOX[j], LOW);
+			if (verbose) {
+				Serial.print("Set sensor low: ");
+				Serial.println(j);
+			}
+		}
+		enable_sensor(i);
+	}
+}
+
+void setup_suspension_sensors() {
+	for (int i = 0; i < NUMBER_OF_SUSP_SENSORS; i++) {
+		pinMode(SHT_LOX[i], OUTPUT);
+	}
+
+	if (verbose) Serial.println(F("Shutdown pins inited..."));
+
+	//write low to pins, enabling reset mode, might not actually be needed here as is done later
+	for (int j = 0; j < NUMBER_OF_SUSP_SENSORS; j++) {
+		digitalWrite(SHT_LOX[j], LOW);
+	}
+
+	if (verbose) Serial.println(F("Both in reset mode...(pins are low)"));
+
+	Serial.println(F("Starting..."));
+	delay(1000);
+	setID();
+}
+
+
 void write_sensor_data() {
 	HCRTC.RTCRead(I2CDS1307Add); //recieve real time clock data
+
+	bool debug = false;  // pass in 'true' to get debug data printout!
+	for (int j = 0; j < NUMBER_OF_SUSP_SENSORS; j++) {
+		lox[j].rangingTest(&measure[j], debug);
+	}
 
 	if (verbose) {
 		Serial.print(HCRTC.GetDateString());
@@ -95,7 +191,7 @@ void write_sensor_data() {
 	delay(25); //program restars if this delay does not exist, 25 was as short as i could get it 
 
 	// (i < (NUMBER_OF_SENSORS - 1))   because we have to run one more sensor than required to ensure the code works correctly, but dont bother trying to read because it will fail
-	for (int i = 0; i < (NUMBER_OF_SENSORS - 1); i++) {
+	for (int i = 0; i < (NUMBER_OF_THERM_SENSORS - 1); i++) {
 		if (therm[i].read()) {
 			if (verbose) {
 				Serial.println(i);
@@ -119,6 +215,22 @@ void write_sensor_data() {
 		}
 		DataFile.print(",");
 	}
+
+	for (int k = 0; k < NUMBER_OF_SUSP_SENSORS; k++) {
+		if (measure[k].RangeStatus != 4) {     // if not out of range
+			DataFile.print(measure[k].RangeMilliMeter);
+			if(verbose) Serial.print(measure[k].RangeMilliMeter);
+		}
+		else {
+			if (verbose) {
+				DataFile.print("error: range");
+				Serial.print("error: range");
+			}
+		}
+		DataFile.print(",");
+		if(verbose) Serial.print(",");
+	}
+
 	DataFile.println();
 	DataFile.close();
 }
